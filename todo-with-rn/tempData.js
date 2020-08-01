c5a7588ec27d54c74277d2ba25a6209660482a3d
Vinicius <viniciusdesouto@gmail.com>
2020-08-01 04:10:18 -0300

    first commit

---
+++ todo-with-rn/tempData.js
@@ -0,0 +1,69 @@
+// temporary data until we connect with firebase
+
+export default tempData = [
+    {
+        id: 1,
+        name: "Plan a Trip",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "Errands",
+        color: "#8022D9",
+        todos: [
+            {
+                title: "Buy Milk",
+                completed: false
+            },
+            {
+                title: "Plan weekend camping trip",
+                completed: true
+            },
+            {
+                title: "Run 2 miles",
+                completed: true
+            },
+            {
+                title: "Take dog for a walk",
+                completed: true
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "Birthday Party",
+        color: "#595BD9",
+        todos: [
+            {
+                title: "Get Balloons",
+                completed: false
+            },
+            {
+                title: "Send Invitations",
+                completed: true
+            },
+            {
+                title: "Make Dinner Reservations",
+                completed: true
+            },
+        ]
+    },
+];
